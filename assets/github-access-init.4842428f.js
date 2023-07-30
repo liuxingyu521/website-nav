a39021ea7e3e5f066e07f021feb85916a75ea8d1
liuxingyu521 <xuj_power@qq.com>
2023-07-30 07:58:58 +0000

    Deploying to gh-pages from @ liuxingyu521/website-nav@f26bebfe54b342bc21b9be028d2f5896e9656c2b 🚀

---
+++ assets/github-access-init.4842428f.js
@@ -0,0 +1,1 @@
+import{u as t,a as o,g as i}from"./app.5c1b4048.js";(async function(){const e=Object.fromEntries(new URL(location.href).searchParams),a=t(),s=o();if(e.code){const c=await i(e.code);c&&(a.value=c,s.value=!0,history.replaceState(null,document.title,`${location.origin}${location.pathname}`))}})();
