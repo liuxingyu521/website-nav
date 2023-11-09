353111732ecc9fdc8c2dc441b5231fd3f2489f6e
liuxingyu521 <xuj_power@qq.com>
2023-11-09 21:50:27 +0800

    chore: analyze build conditional

--- vite.config.js
+++ vite.config.js
@@ -8,40 +8,48 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/',
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src/'),
-    },
-  },
-  plugins: [
-    vue(),
-    content({
-      YAML: {
-        enable: true,
+export default defineConfig((options) => {
+  const env = process.env
+  const isEnableAnalyze = env.ANALYZE === 'true'
+
+  return {
+    base: '/',
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, 'src/'),
       },
-    }),
-    AutoImport({
-      resolvers: [ElementPlusResolver()],
-      imports: [
-        {
-          '@vueuse/core': [
-            // named imports
-            'useFetch',
-          ],
+    },
+    plugins: [
+      vue(),
+      content({
+        YAML: {
+          enable: true,
         },
-      ],
-    }),
-    Components({
-      resolvers: [ElementPlusResolver()],
-    }),
-    visualizer(),
-  ],
-  server: {
-    port: 3000,
-  },
-  ssr: {
-    noExternal: ['element-plus'],
-  },
+      }),
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+        imports: [
+          {
+            '@vueuse/core': [
+              // named imports
+              'useFetch',
+            ],
+          },
+        ],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+      isEnableAnalyze && visualizer({
+        open: true,
+        gzipSize: true,
+      }),
+    ],
+    server: {
+      port: 3000,
+    },
+    ssr: {
+      noExternal: ['element-plus'],
+    },
+  }
 })
